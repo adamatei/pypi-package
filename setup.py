3fb68c3f73accbd2ba31e758da49c2a071d00525
Andreea <a.matei@student.fontys.nl>
2022-06-22 10:06:49 +0200

    disc added

--- setup.py
+++ setup.py
@@ -11,12 +11,12 @@ setup(
     package_dir={'': 'src'},
     url='https://github.com/adamatei/pypi-package',
     keywords='test project',
-    python_requires="=3.10.0",
+    python_requires="==3.10.0",
     install_requires=[
         'flask-login',
         'setuptools',
-        'six=1.16.0',
-        'sqlite=3.36',
+        'six==1.16.0',
+        'sqlite==3.36',
         'flask==2.0.2',
         'colorama==0.4.4',
         'pymata4==1.15'
